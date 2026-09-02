566ad4ec7621287014778ad5965e18ddae11a492
Scott Harding <scott.harding@maine.edu>
2026-09-02 13:08:02 -0400

    second try

--- Pet.java
+++ Pet.java
@@ -2,7 +2,7 @@
 public abstract class Pet {
 	
 	private String diet="";
-	private boolean nocturnal=false;
+	private boolean nocturnal=true;
 	private boolean poisonous=false;
 	private boolean flying=false;
 
